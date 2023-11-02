c0d66623d2c5440186eeeb66a75df5956a0d05a0
Hong <wnsvy1237@naver.com>
2023-11-02 13:36:14 +0900

    드위터 초기버전

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import tweetRouter from "./router/tweets.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+
+// 라우터
+
+app.use("/tweets", tweetRouter);
+
+
+// no SQL?
+
+
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.listen(8080);

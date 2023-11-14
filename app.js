39c8eea81422b72321953b62e7cff3c4480ea154
Hong <wnsvy1237@naver.com>
2023-11-14 11:26:58 +0900

    sequelize 적용

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import { config } from "./config.js";
 // dotenv.config();
 import cors from "cors";
 import { initSocket } from "./connection/socket.js";
-import { db } from "./db/database.js"
+import { sequelize } from "./db/database.js"
+
+
 
 
 console.log(process.env.JWT_SECRET);
@@ -28,8 +30,12 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
+sequelize.sync().then(() => {
+    const server = app.listen(config.host.port);
+    initSocket(server);
+ } );
+
 
 // db.getConnection().then(connection => console.log(connection));      커넥션이 이루어졌는지 확인하는 용도
-const server = app.listen(config.host.port);
-initSocket(server);
+
 

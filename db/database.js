c4b9c74219d912bb5b8edfae9f5e5729dacd86e6
Hong <wnsvy1237@naver.com>
2023-11-16 13:58:06 +0900

    MongoDB 적용

--- db/database.js
+++ db/database.js
@@ -1,11 +1,29 @@
-import mysql from "mysql2";
+// import mysql from "mysql2";
+// import { config } from "../config.js";
+
+// const pool = mysql.createPool({
+//     host: config.db.host,
+//     user: config.db.user,
+//     database : config.db.database,
+//     password: config.db.password
+// });
+
+// export const db = pool.promise();
+
+import MongoDb from "mongodb";
 import { config } from "../config.js";
 
-const pool = mysql.createPool({
-    host: config.db.host,
-    user: config.db.user,
-    database : config.db.database,
-    password: config.db.password
-});
+let db;
 
-export const db = pool.promise();
+export async function connectDB(){
+    return MongoDb.MongoClient.connect(config.db.host).then((client) => (db = client.db())); //몽고db 클라우드 연결;
+}
+
+export function getUsers() {
+    return db.collection("users");
+}
+
+export function getTweets() {
+    return db.collection("tweets");
+}
+

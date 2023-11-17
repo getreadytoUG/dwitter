ecbae28f2023645f1f3d9b3f4a19daa5930f09dd
Hong <wnsvy1237@naver.com>
2023-11-17 10:29:38 +0900

    Mongoose 적용

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import { config } from "./config.js";
 // dotenv.config();
 import cors from "cors";
 import { initSocket } from "./connection/socket.js";
-import { db } from "./db/database.js"
+// import { db } from "./db/database.js"
+import { connectDB } from "./db/database.js"
 
 
 console.log(process.env.JWT_SECRET);
@@ -30,6 +31,10 @@ app.use((req, res, next) => {
 
 
 // db.getConnection().then(connection => console.log(connection));      커넥션이 이루어졌는지 확인하는 용도
-const server = app.listen(config.host.port);
-initSocket(server);
+
+connectDB().then(() => {
+    const server = app.listen(config.host.port);
+    initSocket(server);
+}).catch(console.error);
+
 
--- data/auth.js
+++ data/auth.js
@@ -1,17 +1,44 @@
-import { db } from "../db/database.js";
+// import { db } from "../db/database.js";
 
-export async function findByUsername(username) {
-    return db.execute("SELECT * FROM users WHERE username = ?", [username]).then((result) => result[0][0]);
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.js";
+
+const userSchema = new Mongoose.Schema({
+    username: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, requried: true },
+    password: { type: String, requried: true },
+    url: String
+})
+
+useVirtualId(userSchema);
+const User = Mongoose.model("User", userSchema);
+
+export async function findByUsername(username){
+    return User.findOne({ username })
 }
 
-export async function findById (id) {
-    return db.execute(`SELECT * FROM users WHERE id = ${id}`).then((result) => result[0][0])
+export async function findById(id){
+    return User.findById(id);       // MongoDB 메서드
 }
 
 export async function createUser(user){
-    const { username, password, name, email, url } = user;    
-    return db.execute("INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)", [username, password, name, email, url]).then((result) => result[0].insertId);
+    return new User(user).save().then((data) => data.id);       // virtual id 가 불러와짐
 }
+
+
+// export async function findByUsername(username) {
+//     return db.execute("SELECT * FROM users WHERE username = ?", [username]).then((result) => result[0][0]);
+// }
+
+// export async function findById (id) {
+//     return db.execute(`SELECT * FROM users WHERE id = ${id}`).then((result) => result[0][0])
+// }
+
+// export async function createUser(user){
+//     const { username, password, name, email, url } = user;    
+//     return db.execute("INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)", [username, password, name, email, url]).then((result) => result[0].insertId);
+// }
 
 
 
